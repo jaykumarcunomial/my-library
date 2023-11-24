0bb9791981c964c5162f06508af4330c63c13489
jaykumarcunomial <105623687+jaykumarcunomial@users.noreply.github.com>
2023-11-24 08:11:38 +0530

    added tailwind with storybook addons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import "../../styles/tailwind.css";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <button
+      type="button"
+      className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
